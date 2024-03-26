da251846068d386413aaad85ea3942fec200e556
Nabin Hamal <nabinhamal2@gmail.com>
2024-03-26 10:33:02 +0545

    home page ui completed

--- app/(tabs)/basket/index.js
+++ app/(tabs)/basket/index.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Redirect } from 'expo-router'
 
 const index = () => {
   return (
-   <Redirect href={"/(tabs)/home"}/>
+    <View>
+      <Text>Basket
+      </Text>
+    </View>
   )
 }
 
--- app/(tabs)/orders/index.js
+++ app/(tabs)/orders/index.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Redirect } from 'expo-router'
 
 const index = () => {
   return (
-   <Redirect href={"/(tabs)/home"}/>
+    <View>
+      <Text>Order</Text>
+    </View>
   )
 }
 
